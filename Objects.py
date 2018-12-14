bef1664bc7ad34d59142889f7b97a222ee3c04b7
eurazov <45424660+eurazov@users.noreply.github.com>
2018-12-14 18:51:58 +0500

    Add files via upload

--- Objects.py
+++ Objects.py
@@ -1,10 +1,6 @@
 import numpy as np
 
 
-def dist(pos1, pos2):
-    return np.sqrt(np.sum((pos1-pos2)**2))
-
-
 class Object:
     def __init__(self, canvas, m, r, p, v=np.array([0, 0]), f=np.array([0, 0], dtype=float)):
         self.mass=m
@@ -13,17 +9,32 @@ class Object:
         self.velocity=v
         self.force=f
         self.id=canvas.create_oval(np.round(self.position[0])-self.radius, np.round(self.position[1])-self.radius, np.round(self.position[0])+self.radius, np.round(self.position[1])+self.radius, fill="black")
+
     def interact(self, obj2):
-        d=dist(self.position,obj2.position)
+        d=self.dist(obj2)
         F=float(self.mass*obj2.mass/(d**2))*10**3
         force_x=F*(obj2.position[0]-self.position[0])/d
         force_y=F*(obj2.position[1]-self.position[1])/d
         self.force+=np.array([force_x, force_y], dtype=float)
         obj2.force-=np.array([force_x, force_y], dtype=float)
+
     def move(self, canvas, dt):
         self.position+=self.velocity*dt
         canvas.move(self.id, np.round(self.velocity[0]*dt), np.round(self.velocity[1]*dt))
+
     def accelerate(self, dt):
         self.velocity+=self.force*dt/self.mass
 
+    def collision(self, b):
+        r=self.radius+b.radius>=self.dist(b)
+        if not r:
+            return r
+        self.velocity = (self.mass * self.velocity + b.mass * b.velocity) / (self.mass + b.mass)
+        self.mass += b.mass
+        self.radius = np.round((self.radius ** 3 + b.radius ** 3) ** (1. / 3))
+        self.position += b.position
+        self.position /= 2
+        return r
 
+    def dist(self, obj2):
+        return np.sqrt(np.sum((self.position - obj2.position) ** 2))
--- Setup.py
+++ Setup.py
@@ -1,18 +1,132 @@
 from tkinter import *
 import numpy as np
 import Objects
-n=2
-#wait=IntVar()
+import time
 
 
 class Setup:
     def __init__(self, root, canvas):
-        global n
-        global wait
-        self.F=initial(root)
-        self.n=n
-        self.a=object_list(root, canvas)
+        canvas.pack_forget()
+        self.n=2
+        self.F = self.initial(root)
+        canvas.config(state=NORMAL)
+        self.a = self.object_list(root, canvas)
 
+    def initial(self, root):
+        def get_amount():
+            F[0] = float(entry_x.get())
+            F[1] = float(entry_y.get())
+            w.destroy()
+
+        F = np.array([0, 0], dtype=float)
+        w = Frame(root)
+        w.pack()
+        okbutton = Button(w, text="OK", command=get_amount)
+        okbutton.pack()
+        label_set_force = Label(w, text="Enter the constant force value.")
+        label_set_force.pack()
+        label_x = Label(w, text="x")
+        label_x.pack(side="left")
+        label_y = Label(w, text="y")
+        label_y.pack(side="right")
+        entry_x = Entry(w)
+        entry_x.insert(0, str(F[0]))
+        entry_x.pack(side="left")
+        entry_y = Entry(w)
+        entry_y.insert(0, str(F[0]))
+        entry_y.pack(side="left")
+        w.wait_window()
+        return F
+    
+    def object_list(self, root, canvas):
+        canvas.pack(fill="both", expand=True)
+        a=[]
+        def start():
+            w.destroy()
+            
+        def random():
+            def randomise():
+                self.n=int(entry.get())
+                for i in range(self.n):
+                    m=np.random.randint(1, 5000)
+                    r=np.random.randint(1, 10)
+                    p=np.array(np.random.randint(1, 450, 2), dtype=float)
+                    v=np.array(np.random.randint(1, 50, 2), dtype=float)
+                    a.append(Objects.Object(canvas, m, r, p, v))
+                canvas.pack(fill="both", expand=True)
+                start()
+            np.random.seed(int(time.time()))
+            
+            canvas.pack_forget()
+            add_button.destroy()
+            start_button.destroy()
+            random_button.destroy()
+            
+            label=Label(w, text="Enter the amount of objects").pack()
+            entry=Entry(w)
+            entry.insert(0, str(self.n))
+            entry.pack(fill="both", expand=True)
+            okbutton=Button(w, text="OK", command=randomise)
+            okbutton.pack()
+            
+        w=Frame(root)
+        w.pack()
+        
+        def add_object():
+            w2=Frame(root)
+            w2.pack()
+            start_button.config(state=DISABLED)
+            add_button.config(state=DISABLED)
+            random_button.config(state=DISABLED)
+            def click(e):
+                def get_param():
+                    a.append(Objects.Object(canvas, float(entry_m.get()), float(entry_r.get()), np.array([canvas.canvasx(e.x), canvas.canvasy(e.y)], dtype=float), np.array([float(entry_Vx.get()), float(entry_Vy.get())]), np.array(self.F, dtype=float)))
+                    w2.destroy()
+                    start_button.config(state=NORMAL)
+                    add_button.config(state=NORMAL)
+                    random_button.config(state=NORMAL)
+                    
+                def cancel():
+                    start_button.config(state=NORMAL)
+                    add_button.config(state=NORMAL)
+                    random_button.config(state=NORMAL)
+                    w2.destroy()
+                    
+                #x=e.x
+                #y=e.y
+                canvas.unbind(funcid)
+                label_m = Label(w2, text="mass").grid(row=0, column=0)
+                label_Vx = Label(w2, text="Vx").grid(row=0, column=1)
+                label_Vy = Label(w2, text="Vy").grid(row=0, column=2)
+                label_r = Label(w2, text="R").grid(row=0, column=3)
+                entry_m=Entry(w2, width=10)
+                entry_m.grid(row=1, column=0)
+                entry_Vx=Entry(w2, width=10)
+                entry_Vx.insert(0, '0')
+                entry_Vx.grid(row=1, column=1)
+                entry_Vy= Entry(w2, width=10)
+                entry_Vy.insert(0, '0')
+                entry_Vy.grid(row=1, column=2)
+                entry_r = Entry(w2, width=10)
+                entry_r.insert(0, '5')
+                entry_r.grid(row=1, column=3)
+                okbutton=Button(w2, text="OK", command=get_param)
+                cancel_button=Button(w2, text="Cancel", command=cancel)
+                okbutton.grid(row=2, column=1)
+                cancel_button.grid(row=2, column=2)
+
+            funcid=canvas.bind("<Button-1>", click)
+            return 0
+        random_button=Button(w, text="Random", command=random)
+        add_button=Button(w, text="Add object", command=add_object)
+        add_button.pack()
+        random_button.pack()
+        start_button=Button(w, text="Start", command=start)
+        start_button.pack()
+        
+            
+        w.wait_window()
+        return a
 
 class param:
     def __init__(self, w, i):
@@ -31,66 +145,3 @@ class param:
         self.r =Entry(w, width=10)
         self.r.insert(0, '5')
         self.r.grid(row=i+1, column=6)
-
-
-def initial(root):
-    def get_amount():
-        global n
-        n = int(entry_amount.get())
-        F[0]=float(entry_x.get())
-        F[1]=float(entry_y.get())
-        #destroy the fields
-        w.destroy()
-        #wait=False
-    F=np.array([0, 0], dtype=float)
-    w=Frame(root)
-    w.pack()
-    label_set_amount = Label(w, text="Choose the amount of objects.")
-    label_set_amount.pack()
-    entry_amount = Entry(w)
-    entry_amount.focus_set()
-    entry_amount.insert(0, str(n))
-    entry_amount.pack()
-    okbutton=Button(w, text="OK", command=get_amount)
-    okbutton.pack()
-    label_set_force=Label(w, text="Enter the constant force value.")
-    label_set_force.pack()
-    label_x=Label(w, text="x")
-    label_x.pack(side="left")
-    label_y=Label(w, text="y")
-    label_y.pack(side="right")
-    entry_x=Entry(w)
-    entry_x.insert(0, '0')
-    entry_x.pack(side="left")
-    entry_y=Entry(w)
-    entry_y.insert(0, '0')
-    entry_y.pack(side="left")
-    w.wait_window()
-    return F
-
-
-def object_list(root, canvas):
-    def get_param():
-        for i in entries:
-            a.append(Objects.Object(canvas, float(i.m.get()), float(i.r.get()), np.array([float(i.x.get()), float(i.y.get())]), np.array([float(i.Vx.get()),float(i.Vy.get())])))
-        w.destroy()
-
-    global n
-    a=[]
-    w=Frame(root)
-    w.pack()
-    label_m =Label(w, text="mass").grid(row=0, column=1)
-    label_x =Label(w, text="x").grid(row=0, column=2)
-    label_y =Label(w, text="y").grid(row=0, column=3)
-    label_Vx=Label(w, text="Vx").grid(row=0, column=4)
-    label_Vy=Label(w, text="Vy").grid(row=0, column=5)
-    label_r =Label(w, text="R").grid(row=0, column=6)
-    label_obj=[]
-    for i in range(n):
-        label_obj.append(Label(w, text=str(i+1)).grid(row=i+1, column=0))
-    entries=[]
-    for i in range(n):
-        entries.append(param(w, i))
-    okbutton=Button(w, text="OK", command=get_param).grid(row=n+2, column=3, columnspan=2)
-    w.wait_window()
-    return a
